99a914e02d6c9209fbf188a91c223f28d56b4c4d
Jack200133 <car20593@uvg.edu.gt>
2021-03-21 15:43:36 -0600

    "Pequeños arreglos"

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         MapFactory mf = new MapFactory();
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Ingrese que tipo de Map se implementara:");
+        System.out.println("1. ");
+        System.out.println("2. LinkedHashMap");
+        System.out.println("3. TreeMap ");
 
 
     }
